78546b3ea75699bb9026f1e91de20188be34787a
lesson1 <b2201980@photon.chitose.ac.jp>
2022-10-30 22:17:40 +0900

    [課題1-3]int型変数ageに任意の値を代入し、その値によって処理を分岐させた

--- src/lecture01/Main.java
+++ src/lecture01/Main.java
@@ -1,10 +1,20 @@
 package lecture01;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int studentNumber = 2201980;
         System.out.println("B" + studentNumber + " Hirakawa Keisuke");
 
+        System.out.println("年齢を入力");
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt();
+        if (age < 20){
+            System.out.printf("I am " + age + " years old so I cannot drink liquor.");
+        }else {
+            System.out.printf("I am " + age + " years old so I can drink liquor.");
+        }
     }
 
 }
