9ec5d6fd8231df878d5fa276a6804896c1edc1f0
lesson1 <b2201980@photon.chitose.ac.jp>
2022-10-31 11:46:20 +0900

    [課題1-2]Kadai2クラスを作成し、int型変数studentNumberに学籍番号の数値の部分を代入し、自分の情報を表示させた

---
+++ src/lecture01/Kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Kadai2 {
+    public static void main(String[] args) {
+        int studentNumber = 2201980;
+        System.out.println("B" + studentNumber + " Hirakawa Keisuke");
+    }
+}
