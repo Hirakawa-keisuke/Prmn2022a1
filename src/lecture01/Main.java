c3443ac066a5df561dc00abac0f31afccfefcb36
lesson1 <b2201980@photon.chitose.ac.jp>
2022-10-31 12:41:52 +0900

    課題ごとにクラスを分けたため、削除した

--- src/lecture01/Main.java
+++ src/lecture01/Main.java
@@ -4,17 +4,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int studentNumber = 2201980;
-        System.out.println("B" + studentNumber + " Hirakawa Keisuke");
-
-        System.out.println("年齢を入力");
-        Scanner scan = new Scanner(System.in);
-        int age = scan.nextInt();
-        if (age < 20){
-            System.out.printf("I am " + age + " years old so I cannot drink liquor.");
-        }else {
-            System.out.printf("I am " + age + " years old so I can drink liquor.");
-        }
-    }
 
 }
