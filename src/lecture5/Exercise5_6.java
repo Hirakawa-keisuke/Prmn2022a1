baee81811cfb970f385bdaabdcedaccf1b139b5e
lesson1 <b2201980@photon.chitose.ac.jp>
2022-12-06 18:53:34 +0900

    [課題5-6]クラス名間違いを修正

--- src/lecture5/Exercise5_6.java
+++ src/lecture5/Exercise5_6.java
@@ -1,6 +1,6 @@
 package lecture5;
 
-public class Exercise5_5 {
+public class Exercise5_6 {
     public static void main(String[] args) {
         SwallowtailButterfly swallowtailButterfly = new SwallowtailButterfly();
         swallowtailButterfly.move();
