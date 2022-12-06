1d4e875c7f16e197b9fc6516907bc268999a698c
lesson1 <b2201980@photon.chitose.ac.jp>
2022-12-06 18:49:36 +0900

    [課題5-6]SwallowtailButterflyクラスをButterflyクラスから継承した

---
+++ src/lecture5/Exercise5_5.java
@@ -0,0 +1,8 @@
+package lecture5;
+
+public class Exercise5_5 {
+    public static void main(String[] args) {
+        SwallowtailButterfly swallowtailButterfly = new SwallowtailButterfly();
+        swallowtailButterfly.move();
+    }
+}
