f03889f08a9ea60d058a072e4e5f1afaa2af82ea
lesson1 <b2201980@photon.chitose.ac.jp>
2022-12-07 10:57:26 +0900

    [課題5-7]インスタンスをリストに格納し、for文でmoveメソッドを呼び出せるようにした

---
+++ src/lecture6/Exercise6_1.java
@@ -0,0 +1,17 @@
+package lecture6;
+
+import java.util.Scanner;
+
+public class Exercise6_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("小数値を入力してください:");
+        try {
+            var scna = scanner.nextDouble();
+            System.out.println("入力した値: " + scna);
+        }catch (Exception e){
+            System.out.println("エラー.");
+        }
+
+    }
+}
